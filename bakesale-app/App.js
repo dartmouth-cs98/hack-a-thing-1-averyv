9d2ff3f50346915acfe76a75847b5610218bd466
Avery Vanacore <avery.e.vanacore.20@dartmouth.edu>
2020-01-12 20:08:52 -0500

    preliminary fetch of the API data

--- bakesale-app/App.js
+++ bakesale-app/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import {View, Text, StyleSheet} from 'react-native';
+import ajax from './src/ajax';
+import DealList from './src/components/DealList';
+// disable really annoying in app warnings
+console.disableYellowBox = true;
+
+class App extends React.Component {
+  state = {
+    deals: [],
+  };
+  async componentDidMount() {
+    const deals = await ajax.fetchInitialDeals();
+    this.setState({deals});
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {
+          this.state.deals.length > 0 ? (
+            <DealList deals={this.state.deals}></DealList> 
+            ) :(
+            <Text style={styles.header}>Bakesale</Text>)}
+      </View>
+    )
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -16,4 +33,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 40,
+  }
 });
+
+export default App;
+
+
